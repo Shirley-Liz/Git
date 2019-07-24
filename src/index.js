1cd837a1adcd5dda3ed9e013cc0645d85a611c45
shirley-liz <shirleymbabazi@gmail.com>
2019-07-24 02:09:12 -0700

    Updates for now

--- src/index.js
+++ src/index.js
@@ -1,41 +1,39 @@
+// *check if the like button is clicked
+//count the number of times the button is clicked
+//send the number of times to display
 
+var buttonlike= document.getElementById("like");
+if(buttonlike){
+    var counter = 0;
+    buttonlike.onclick = function increment(event){
+        const inc = document.getElementById("increase")
+        if(inc){
+        event.preventDefault();
 
-function displayCurrentDt() {
-    var mydate = new Date();
-    var year = mydate.getFullYear();
-    if(year < 1000){
-        year +=1900;
+        counter++;
+        var numberOfLikes = counter === 1
+            ? counter.toString() + "like"
+            : counter.toString() + "likes"
+        
+        document.getElementById("increase").innerHTML = numberOfLikes
+       
+    } else{
+        console.warn("you have an error");
     }
-var day = mydate.getDay();
-var month = myDate.getMonth();
-var daym = myDate.getDate();
-var dayarray = new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday");
-var montharray = new Array("January","February","March","April","May","June","July","August","September","October","November","December"); 
-  var currentTime = newDate();
-  var h = currentTime.getHours();
-  var m = currentTime.getMinutes();
-  var s = currentTime.getSeconds();
-  if(h == 24){
-      h=0;
-  } else if (h > 12){
-      h = h - 0;
-  }
-   if (h < 10){
-       h = "0" + h;
-   }
-   if (m < 10){
-       m = "0" + m;
-   }
-   if (s < 10){
-       s = "0" + s;
-   }
-var myClock = document.getElementById("timedate");
-myClock.textcontent = "" +dayarray[day]+ "" +daym+ "" +montharray[month]+ "" +year+ "|" +h+ ":" +m+ ":" +s;
-myclock.innerText =  "" +dayarray[day]+ "" +daym+ "" +montharray[month]+ "" +year+ "|" +h+ ":" +m+ ":" +s;
-setTimeout("displayCurrentDt()", 1000);
-
 }
-displayCurrentDt();
-// function displayDate(){
-// document.getElementById(timedate).innerHTML = Date()
-//}
+}
+else{
+    console.warn("you have an error");
+}
+
+const comm = document.getElementById("commentbutton")
+comm.addEventListener("click", addNewComment);
+function addNewComment(event){
+    event.preventDefault();
+    // var newComment = {
+    //   "comment": document.getElementById("comment1").value
+     
+    // }
+    alert("Hello");
+}
+
